44a6d638f2b7f4b2d63587c50dc76c60c3563933
AbhishekSharma20 <asharma20011994@gmail.com>
2016-10-23 19:02:57 +0530

    Add files via upload

---
+++ js/resumeBuilder.js
@@ -0,0 +1,47 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ 
+ //$("#main").append("abhishek");
+ var role = "Web Developer";
+ var formattedName = HTMLheaderName.replace("%data%","Abhishek");
+ var formattedRole = HTMLheaderRole.replace("%data%",role);
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
+ var skills = ["Programming","JS","C#"];
+ $("#main").append(skills);
+ var bio = {
+ 	"name":"Abhishek",
+ 	"role":"Consultant",
+ 	"PictureURL":"images/fry.jpg",
+ 	"contact info": "+918013058180",
+ 	"Welcome message": "Hi!",
+ 	"skills": skills
+ };
+
+
+
+
+$("#main").append(work["position"]);
+$("#main").append(education.city);
+*/
+
+var work = {};
+work.position = "Consultant";
+work.employer = "PwC";
+work.years = 1;
+work.city = "Kolkata";
+
+var education={};
+education["school"]="IIEST,Shibpur";
+education["year"]=2016;
+education["city"]="Howrah";
+
+var bio={}
+bio.name = "Abhishek";
+bio.DOB = "20/01/1994";
+
+var project={}
+project.title="Reversible Logic";
+project.Mentor="H Rahaman";
+
+
